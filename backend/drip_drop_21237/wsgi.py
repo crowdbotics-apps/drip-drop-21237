939e676d8f2aaf0f69e1c238d2a525e14c7f39f9
Crowdbotics <team@crowdbotics.com>
2020-10-07 23:09:57 +0000

    Update models

--- backend/drip_drop_21237/wsgi.py
+++ backend/drip_drop_21237/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drip_drop_21237.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drip_drop_21237.settings")
 
 application = get_wsgi_application()
